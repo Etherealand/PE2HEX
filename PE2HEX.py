c4cc472be152bb4818c287c49a4adb16a2135807
Etherealand <67529048+Etherealand@users.noreply.github.com>
2021-01-07 16:29:52 +0800

    Update PE2HEX.py

--- PE2HEX.py
+++ PE2HEX.py
@@ -8,7 +8,7 @@ parser.add_argument('--out', default='output.txt', type=str,
                     help='Output filename or full path')
 parser.add_argument('--key', default='\0', type=str,
                     help='Encryption key ascii string')
-print(args.)
+print(args.input)
 args = parser.parse_args()
 file = bytearray(open(args.input, 'rb').read())
 
